31d61207a4576e23a06d0dbd0b00bce120d7486a
Abdelrahman Alshater <shater_11@hotmail.com>
2023-06-12 13:45:52 +0300

    date added

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,12 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, data, title}) => (
+          {allPostsData.map(({ id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
-                {/* <Date dateString={date} /> */}
+                <Date dateString={date} />
               </small>
             </li>
           ))}
